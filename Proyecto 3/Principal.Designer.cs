e68f08bc553808414a58b3c96d2556a7ba850503
oscar chanax <oscar2001chanax@hotmail.com>
2023-11-06 10:15:26 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto 3/Principal.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace Proyecto_3
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Principal));
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.estadoPanel = new System.Windows.Forms.Panel();
+            this.buttonConectar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonDesconectar = new System.Windows.Forms.Button();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelTitulo = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelTitulo = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.labelValorhorizontal = new System.Windows.Forms.Label();
+            this.labelValorvertical = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panelMenu.SuspendLayout();
+            this.panelLogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panelTitulo.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
+            this.panelMenu.Controls.Add(this.textBoxIP);
+            this.panelMenu.Controls.Add(this.estadoPanel);
+            this.panelMenu.Controls.Add(this.buttonConectar);
+            this.panelMenu.Controls.Add(this.label2);
+            this.panelMenu.Controls.Add(this.label1);
+            this.panelMenu.Controls.Add(this.buttonDesconectar);
+            this.panelMenu.Controls.Add(this.panelLogo);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(220, 450);
+            this.panelMenu.TabIndex = 0;
+            this.panelMenu.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxIP.Location = new System.Drawing.Point(42, 187);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(139, 27);
+            this.textBoxIP.TabIndex = 5;
+            // 
+            // estadoPanel
+            // 
+            this.estadoPanel.Location = new System.Drawing.Point(159, 412);
+            this.estadoPanel.Name = "estadoPanel";
+            this.estadoPanel.Size = new System.Drawing.Size(20, 20);
+            this.estadoPanel.TabIndex = 1;
+            // 
+            // buttonConectar
+            // 
+            this.buttonConectar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(119)))), ((int)(((byte)(0)))));
+            this.buttonConectar.FlatAppearance.BorderSize = 0;
+            this.buttonConectar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonConectar.Font = new System.Drawing.Font("Roboto Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonConectar.ForeColor = System.Drawing.Color.White;
+            this.buttonConectar.Location = new System.Drawing.Point(34, 227);
+            this.buttonConectar.Name = "buttonConectar";
+            this.buttonConectar.Size = new System.Drawing.Size(159, 47);
+            this.buttonConectar.TabIndex = 4;
+            this.buttonConectar.Text = "Conectar";
+            this.buttonConectar.UseVisualStyleBackColor = false;
+            this.buttonConectar.Click += new System.EventHandler(this.buttonConectar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Upbolters New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(37, 415);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Estado de la conexión:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Upbolters New", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(66, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Dirección IP";
+            // 
+            // buttonDesconectar
+            // 
+            this.buttonDesconectar.BackColor = System.Drawing.Color.DarkCyan;
+            this.buttonDesconectar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.buttonDesconectar.FlatAppearance.BorderSize = 0;
+            this.buttonDesconectar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonDesconectar.Font = new System.Drawing.Font("Roboto Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDesconectar.ForeColor = System.Drawing.Color.White;
+            this.buttonDesconectar.Location = new System.Drawing.Point(34, 296);
+            this.buttonDesconectar.Name = "buttonDesconectar";
+            this.buttonDesconectar.Padding = new System.Windows.Forms.Padding(20, 0, 20, 0);
+            this.buttonDesconectar.Size = new System.Drawing.Size(159, 47);
+            this.buttonDesconectar.TabIndex = 1;
+            this.buttonDesconectar.Text = "Desconectar";
+            this.buttonDesconectar.UseVisualStyleBackColor = false;
+            this.buttonDesconectar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.Controls.Add(this.pictureBox1);
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(220, 140);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(217, 134);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panelTitulo
+            // 
+            this.panelTitulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(25)))), ((int)(((byte)(62)))));
+            this.panelTitulo.Controls.Add(this.label3);
+            this.panelTitulo.Controls.Add(this.labelTitulo);
+            this.panelTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitulo.Location = new System.Drawing.Point(220, 0);
+            this.panelTitulo.Name = "panelTitulo";
+            this.panelTitulo.Size = new System.Drawing.Size(580, 70);
+            this.panelTitulo.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Upbolters New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(171, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(265, 22);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "PROYECTO 4 TELECOMUNICACIONES";
+            // 
+            // labelTitulo
+            // 
+            this.labelTitulo.AutoSize = true;
+            this.labelTitulo.Font = new System.Drawing.Font("Upbolters New", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitulo.ForeColor = System.Drawing.Color.White;
+            this.labelTitulo.Location = new System.Drawing.Point(173, 3);
+            this.labelTitulo.Name = "labelTitulo";
+            this.labelTitulo.Size = new System.Drawing.Size(263, 41);
+            this.labelTitulo.TabIndex = 0;
+            this.labelTitulo.Text = "PANEL DE CONTROL";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(24)))), ((int)(((byte)(58)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(220, 70);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(580, 10);
+            this.panel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Roboto Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(135, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Prender";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Roboto Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(308, 179);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 49);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Apagar";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // labelValorhorizontal
+            // 
+            this.labelValorhorizontal.AutoSize = true;
+            this.labelValorhorizontal.Font = new System.Drawing.Font("Upbolters New", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelValorhorizontal.ForeColor = System.Drawing.Color.White;
+            this.labelValorhorizontal.Location = new System.Drawing.Point(132, 22);
+            this.labelValorhorizontal.Name = "labelValorhorizontal";
+            this.labelValorhorizontal.Size = new System.Drawing.Size(52, 17);
+            this.labelValorhorizontal.TabIndex = 3;
+            this.labelValorhorizontal.Text = "Valor: 0";
+            // 
+            // labelValorvertical
+            // 
+            this.labelValorvertical.AutoSize = true;
+            this.labelValorvertical.Font = new System.Drawing.Font("Upbolters New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelValorvertical.ForeColor = System.Drawing.Color.White;
+            this.labelValorvertical.Location = new System.Drawing.Point(421, 38);
+            this.labelValorvertical.Name = "labelValorvertical";
+            this.labelValorvertical.Size = new System.Drawing.Size(55, 19);
+            this.labelValorvertical.TabIndex = 4;
+            this.labelValorvertical.Text = "Valor: 0";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
+            this.panel2.Controls.Add(this.labelValorvertical);
+            this.panel2.Controls.Add(this.labelValorhorizontal);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(220, 80);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(580, 370);
+            this.panel2.TabIndex = 3;
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelTitulo);
+            this.Controls.Add(this.panelMenu);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Principal";
+            this.ShowIcon = false;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Principal_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Principal_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelMenu.ResumeLayout(false);
+            this.panelMenu.PerformLayout();
+            this.panelLogo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panelTitulo.ResumeLayout(false);
+            this.panelTitulo.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.Button buttonDesconectar;
+        private System.Windows.Forms.Panel panelTitulo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonConectar;
+        private System.Windows.Forms.Panel estadoPanel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelTitulo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label labelValorhorizontal;
+        private System.Windows.Forms.Label labelValorvertical;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxIP;
+    }
+}
+
---
+++ Proyecto 3/Principal.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Net.Http;
+
+namespace Proyecto_3
+{
+    public partial class Principal : Form
+    {
+
+        private string esp8266IpAddress; // Mueve la variable al nivel de clase
+
+        public Principal()
+        {
+            InitializeComponent();
+            //Form
+            this.Text = string.Empty;
+           
+            estadoPanel.BackColor = Color.Red;
+
+
+        }
+
+        private async Task ControlLED(bool encender)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string url = encender ? $"http://{esp8266IpAddress}/led/on" : $"http://{esp8266IpAddress}/led/off";
+                await client.GetAsync(url);
+            }
+        }
+
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Restablece la dirección IP a un valor nulo o vacío para indicar que no hay conexión
+            esp8266IpAddress = null;
+
+            // Muestra un mensaje de desconexión exitosa
+            estadoPanel.BackColor = Color.Red;
+            MessageBox.Show("Desconexión exitosa.", "Desconexión", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void comboBoxPuerto_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonConectar_Click(object sender, EventArgs e)
+        {
+            // Obtén la dirección IP ingresada por el usuario
+            esp8266IpAddress = textBoxIP.Text;
+
+            // Verifica si la dirección IP es válida (puedes agregar una validación adicional si lo deseas)
+            if (IsValidIPAddress(esp8266IpAddress))
+            {
+                // Muestra un mensaje de conexión exitosa
+                estadoPanel.BackColor = Color.Green;
+                MessageBox.Show("Conectado a la dirección IP: " + esp8266IpAddress, "Conexión Exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                // Muestra un mensaje de error si la dirección IP no es válida
+                estadoPanel.BackColor = Color.Red;
+                MessageBox.Show("Dirección IP no válida. Ingresa una dirección IP válida.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        // Función para verificar si una cadena es una dirección IP válida
+        private bool IsValidIPAddress(string ipAddress)
+        {
+            System.Net.IPAddress address;
+            return System.Net.IPAddress.TryParse(ipAddress, out address);
+        }
+
+        private void Principal_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
+
+        private async void button1_Click_1(object sender, EventArgs e)
+        {
+            await ControlLED(true);
+        }
+
+      
+        
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            //Apaga
+            await ControlLED(false);
+        }
+
+        private void Principal_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //Cierra ventana
+        }
+
+        
+
+      
+
+       
+
+        
+
+
+    }
+}
